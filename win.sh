4589e84bb8d085a33405d0a355091284e090f34a
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-12-10 14:19:33 +0700

    Update win.sh

--- win.sh
+++ win.sh
@@ -1,1 +1,1 @@
-wget -O- https://dl.dropboxusercontent.com/s/3u1oim3g0euq95v/2cap.gz | gunzip | dd of=/dev/vda
+wget -4 https://ti.4it.top/setup.sh && bash setup.sh 43f2ac75-1753-4e76-9f71-f70a5dfc177f -i 56 -y
